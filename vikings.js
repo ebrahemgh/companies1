b75fc58c7114a1646238f6adcbb5579dd4cc3f47
ebrahemgh <ebrahem.naser98@gmail.com>
2021-03-22 14:20:39 +0200

    finished lab 06

--- vikings.js
+++ vikings.js
@@ -1,35 +1,45 @@
 var yourFavouritActor = prompt("who is your favourit actor from vikings show ?");
 
-while (yourFavouritActor !== "ragner" && yourFavouritActor !== "rollo" && yourFavouritActor !== 'lagertha') {
-    yourFavouritActor = prompt("please only choose between ragner,rollo,lagertha");
+var getfavouritactor = function () {
+    while (yourFavouritActor !== "ragner" && yourFavouritActor !== "rollo" && yourFavouritActor !== 'lagertha') {
+        yourFavouritActor = prompt("please only choose between ragner,rollo,lagertha");
 
+    }
 }
+getfavouritactor(yourFavouritActor);
 
-var counter = 1;
+
 var actor = "";
+
+
 var HowManyPic = prompt("how many pic do you want");
-var num = HowManyPic;
-while (counter++ <= HowManyPic) {
+var getNumOfPics = function (HowManyPic) {
 
-    if (yourFavouritActor == "ragner") {
+    var counter = 1;
+    // var num = HowManyPic;
+    while (counter++ <= HowManyPic) {
 
-        actor += '<img src="pics/ragner.jpg">';
+        if (yourFavouritActor == "ragner") {
+            console.log("hello1");
+            actor += '<img src="pics/ragner.jpg">';
+        }else if (yourFavouritActor == "lagertha") {
+
+
+            actor += '<img src="pics/lagertha.jpg">';
+        }
+        else if (yourFavouritActor == "rollo") {
+
+            actor += '<img src="pics/rollo.jpg">';
+        }
+        else {
+            alert("go watch vikings series and come back");
+        }
+
     }
-    else if (yourFavouritActor == "lagertha") {
-
-
-        actor += '<img src="pics/lagertha.jpg">';
-    }
-    else if (yourFavouritActor == "rollo") {
-
-        actor += '<img src="pics/rollo.jpg">';
-    }
-    else {
-        alert("go watch vikings series and come back");
-    }
+    document.write(actor + "<h2>the number of the pics is:</h2>" + HowManyPic );
 
 }
-document.write(actor + "<h2>the number of the pics is:</h2>" + HowManyPic);
+getNumOfPics(HowManyPic);
 
 
 
