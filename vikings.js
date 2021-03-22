857ac5cf5db56dc2194d1f0d287e738762f789f8
ebrahemgh <ebrahem.naser98@gmail.com>
2021-03-22 20:46:22 +0200

    finished lab 06b

--- vikings.js
+++ vikings.js
@@ -17,26 +17,31 @@ var getNumOfPics = function (HowManyPic) {
 
     var counter = 1;
     // var num = HowManyPic;
+    
     while (counter++ <= HowManyPic) {
 
         if (yourFavouritActor == "ragner") {
             console.log("hello1");
             actor += '<img src="pics/ragner.jpg">';
+            
         }else if (yourFavouritActor == "lagertha") {
 
 
             actor += '<img src="pics/lagertha.jpg">';
+            
         }
         else if (yourFavouritActor == "rollo") {
 
             actor += '<img src="pics/rollo.jpg">';
+            
         }
         else {
             alert("go watch vikings series and come back");
         }
 
     }
-    document.write(actor + "<h2>the number of the pics is:</h2>" + HowManyPic );
+    
+    document.write(actor );
 
 }
 getNumOfPics(HowManyPic);
